34c11270d1d9c6a5677d0ebc4fa754782cfb4d2b
iris-zjdev <zj_shi@outlook.com>
2025-06-13 20:55:37 -0400

    change

--- db_config.py
+++ db_config.py
@@ -1,16 +1,23 @@
-# DB_CONFIG = {
-#     "host": "ep-shy-band-a5vmxekw-pooler.us-east-2.aws.neon.tech",
-#     "database": "neondb",
-#     "user": "neondb_owner",
-#     "password": "npg_3CbjfwlJ0cIx",
-#     "port": 5432,
-#     "sslmode": "require"
-# }
-from sqlalchemy import create_engine
-import os
+import psycopg2
+import urllib.parse
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = "postgresql://neondb_owner:npg_3CbjfwlJ0cIx@ep-shy-band-a5vmxekw-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
 
-# SQLAlchemy engine using pg8000 as the driver
-DATABASE_URL = os.getenv("DATABASE_URL", "").replace("postgresql://", "postgresql+pg8000://")
-engine = create_engine(DATABASE_URL, echo=False, future=True)
+url = urllib.parse.urlparse(DATABASE_URL)
+
+DB_CONFIG = {
+    "host": url.hostname,
+    "database": url.path.lstrip("/"),
+    "user": url.username,
+    "password": url.password,
+    "port": url.port,
+    "sslmode": "require"
+}
+# from sqlalchemy import create_engine
+# import os
+
+# DATABASE_URL = os.environ.get("DATABASE_URL")
+
+# # SQLAlchemy engine using pg8000 as the driver
+# DATABASE_URL = os.getenv("DATABASE_URL", "").replace("postgresql://", "postgresql+pg8000://")
+# engine = create_engine(DATABASE_URL, echo=False, future=True)
